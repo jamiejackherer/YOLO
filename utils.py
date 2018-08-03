6c1675e5b9e765709bbd8e8c4617ad1af24911a5
Yang Liu <foamliu@yeah.net>
2018-08-03 17:22:09 +0800

    update model

--- utils.py
+++ utils.py
@@ -90,14 +90,18 @@ def filter_boxes(box_confidence, boxes, box_class_probs, threshold=.6):
 
     # Step 1: Compute box scores
     box_scores = box_confidence * box_class_probs
+    print('box_scores.shape: ' + str(box_scores.shape))
 
     # Step 2: Find the box_classes thanks to the max box_scores, keep track of the corresponding score
     box_classes = K.argmax(box_scores, axis=-1)
+    print('box_classes.shape: ' + str(box_classes.shape))
     box_class_scores = K.max(box_scores, axis=-1)
+    print('box_class_scores.shape: ' + str(box_class_scores.shape))
 
     # Step 3: Create a filtering mask based on "box_class_scores" by using "threshold". The mask should have the
     # same dimension as box_class_scores, and be True for the boxes you want to keep (with probability >= threshold)
     filtering_mask = box_class_scores >= threshold
+    print('filtering_mask.shape: ' + str(filtering_mask.shape))
 
     # Step 4: Apply the mask to scores, boxes and classes
     scores = tf.boolean_mask(box_class_scores, filtering_mask)
