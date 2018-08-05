05b73075530c5510c36f74b953aa101104cb32be
Yang Liu <foamliu@yeah.net>
2018-08-05 21:04:55 +0800

    OOM

--- demo.py
+++ demo.py
@@ -9,7 +9,7 @@ import tensorflow as tf
 from config import image_h, image_w, valid_image_folder, max_boxes, iou_threshold, best_model, labels, grid_size, \
     score_threshold
 from model import build_model
-from utils import ensure_folder, filter_boxes, yolo_boxes_to_corners, scale_boxes, sigmoid, update_box_xy
+from utils import ensure_folder, filter_boxes, yolo_boxes_to_corners, scale_boxes, update_box_xy
 
 if __name__ == '__main__':
     model = build_model()
@@ -20,7 +20,7 @@ if __name__ == '__main__':
     test_images = [f for f in os.listdir(test_path) if
                    os.path.isfile(os.path.join(test_path, f)) and f.endswith('.jpg')]
     num_samples = 1
-    #samples = random.sample(test_images, num_samples)
+    # samples = random.sample(test_images, num_samples)
     samples = [test_images[0]]
 
     ensure_folder('images')
@@ -34,17 +34,19 @@ if __name__ == '__main__':
         print('image_shape: ' + str(image_shape))
         image_input = cv.resize(image_bgr, (image_h, image_w), cv.INTER_CUBIC)
         image_input = np.expand_dims(image_input, 0).astype(np.float32)
-        preds = model.predict(image_input)  # [1, 14, 14, 5, 85]
+        preds = model.predict(image_input)  # [1, 14, 14, 85]
         # print('preds: ' + str(preds))
-        box_confidence = sigmoid(preds[0, :, :, :, 0])
+        box_confidence = preds[0, :, :, 0]
         # print('box_confidence: ' + str(box_confidence))
         box_confidence = np.expand_dims(box_confidence, axis=-1)
-        box_xy = sigmoid(preds[0, :, :, :, 1:3])
+        box_confidence = np.clip(box_confidence, 0.0, 1.0)
+        box_xy = preds[0, :, :, 1:3]
+        box_xy = np.clip(box_xy, 0.0, 1.0)
         box_xy = update_box_xy(box_xy)
         # print('box_xy: ' + str(box_xy))
-        box_wh = preds[0, :, :, :, 3:5] * grid_size
+        box_wh = preds[0, :, :, 3:5] * grid_size
         # print('box_wh: ' + str(box_wh))
-        box_class_probs = preds[0, :, :, :, 5:]
+        box_class_probs = preds[0, :, :, 5:]
         boxes = yolo_boxes_to_corners(box_xy, box_wh)
         # print('boxes after to_corners: ' + str(boxes))
         scores, boxes, classes = filter_boxes(box_confidence, boxes, box_class_probs, score_threshold)
