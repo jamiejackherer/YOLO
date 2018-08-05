bea9b151264fb24c8d28cb68ec03479d47983769
Yang Liu <foamliu@yeah.net>
2018-08-06 00:07:19 +0800

    revert to darknet

--- utils.py
+++ utils.py
@@ -11,7 +11,6 @@ from config import train_annot_file, valid_annot_file, lambda_coord, lambda_noob
 
 
 def yolo_loss(y_true, y_pred):
-    length = K.int_shape(y_true)[0]
     conf = y_true[..., 0]  # [None, 14, 14]
     conf = K.expand_dims(conf, axis=-1)  # [None, 14, 14, 1]
     obj_i_mask = tf.to_float(conf == 1.0)  # [None, 14, 14, 1]
@@ -29,7 +28,7 @@ def yolo_loss(y_true, y_pred):
     loss_conf += lambda_noobj * K.sum(noobj_i_mask * K.square(conf - conf_hat))
     loss_class = K.sum(obj_i_mask * K.square(cls - cls_hat))
     total_loss = loss_xy + loss_wh + loss_conf + loss_class
-    return total_loss / length
+    return total_loss
 
 
 def ensure_folder(folder):
