e4943229b07bbfef497624b7de7d8a4ad70cf420
Yang Liu <foamliu@yeah.net>
2018-08-05 22:39:52 +0800

    use image net pretrained model

--- model.py
+++ model.py
@@ -1,139 +1,18 @@
 import keras.backend as K
-from keras.layers import Input, Conv2D, BatchNormalization, LeakyReLU, MaxPooling2D, Lambda, Reshape
-from keras.layers.merge import concatenate
+from keras.applications.vgg16 import VGG16
+from keras.layers import Conv2D, Reshape
 from keras.models import Model
 
 from config import image_h, image_w, num_classes, num_box, num_grid
-from utils import space_to_depth_x2
 
 
 def build_model():
-    input_image = Input(shape=(image_h, image_w, 3))
-
-    # Layer 1
-    x = Conv2D(32, (3, 3), strides=(1, 1), padding='same', name='conv_1', use_bias=False)(input_image)
-    x = BatchNormalization(name='norm_1')(x)
-    x = LeakyReLU(alpha=0.1)(x)
-    x = MaxPooling2D(pool_size=(2, 2))(x)
-
-    # Layer 2
-    x = Conv2D(64, (3, 3), strides=(1, 1), padding='same', name='conv_2', use_bias=False)(x)
-    x = BatchNormalization(name='norm_2')(x)
-    x = LeakyReLU(alpha=0.1)(x)
-    x = MaxPooling2D(pool_size=(2, 2))(x)
-
-    # Layer 3
-    x = Conv2D(128, (3, 3), strides=(1, 1), padding='same', name='conv_3', use_bias=False)(x)
-    x = BatchNormalization(name='norm_3')(x)
-    x = LeakyReLU(alpha=0.1)(x)
-
-    # Layer 4
-    x = Conv2D(64, (1, 1), strides=(1, 1), padding='same', name='conv_4', use_bias=False)(x)
-    x = BatchNormalization(name='norm_4')(x)
-    x = LeakyReLU(alpha=0.1)(x)
-
-    # Layer 5
-    x = Conv2D(128, (3, 3), strides=(1, 1), padding='same', name='conv_5', use_bias=False)(x)
-    x = BatchNormalization(name='norm_5')(x)
-    x = LeakyReLU(alpha=0.1)(x)
-    x = MaxPooling2D(pool_size=(2, 2))(x)
-
-    # Layer 6
-    x = Conv2D(256, (3, 3), strides=(1, 1), padding='same', name='conv_6', use_bias=False)(x)
-    x = BatchNormalization(name='norm_6')(x)
-    x = LeakyReLU(alpha=0.1)(x)
-
-    # Layer 7
-    x = Conv2D(128, (1, 1), strides=(1, 1), padding='same', name='conv_7', use_bias=False)(x)
-    x = BatchNormalization(name='norm_7')(x)
-    x = LeakyReLU(alpha=0.1)(x)
-
-    # Layer 8
-    x = Conv2D(256, (3, 3), strides=(1, 1), padding='same', name='conv_8', use_bias=False)(x)
-    x = BatchNormalization(name='norm_8')(x)
-    x = LeakyReLU(alpha=0.1)(x)
-    x = MaxPooling2D(pool_size=(2, 2))(x)
-
-    # Layer 9
-    x = Conv2D(512, (3, 3), strides=(1, 1), padding='same', name='conv_9', use_bias=False)(x)
-    x = BatchNormalization(name='norm_9')(x)
-    x = LeakyReLU(alpha=0.1)(x)
-
-    # Layer 10
-    x = Conv2D(256, (1, 1), strides=(1, 1), padding='same', name='conv_10', use_bias=False)(x)
-    x = BatchNormalization(name='norm_10')(x)
-    x = LeakyReLU(alpha=0.1)(x)
-
-    # Layer 11
-    x = Conv2D(512, (3, 3), strides=(1, 1), padding='same', name='conv_11', use_bias=False)(x)
-    x = BatchNormalization(name='norm_11')(x)
-    x = LeakyReLU(alpha=0.1)(x)
-
-    # Layer 12
-    x = Conv2D(256, (1, 1), strides=(1, 1), padding='same', name='conv_12', use_bias=False)(x)
-    x = BatchNormalization(name='norm_12')(x)
-    x = LeakyReLU(alpha=0.1)(x)
-
-    # Layer 13
-    x = Conv2D(512, (3, 3), strides=(1, 1), padding='same', name='conv_13', use_bias=False)(x)
-    x = BatchNormalization(name='norm_13')(x)
-    x = LeakyReLU(alpha=0.1)(x)
-
-    skip_connection = x
-
-    x = MaxPooling2D(pool_size=(2, 2))(x)
-
-    # Layer 14
-    x = Conv2D(1024, (3, 3), strides=(1, 1), padding='same', name='conv_14', use_bias=False)(x)
-    x = BatchNormalization(name='norm_14')(x)
-    x = LeakyReLU(alpha=0.1)(x)
-
-    # Layer 15
-    x = Conv2D(512, (1, 1), strides=(1, 1), padding='same', name='conv_15', use_bias=False)(x)
-    x = BatchNormalization(name='norm_15')(x)
-    x = LeakyReLU(alpha=0.1)(x)
-
-    # Layer 16
-    x = Conv2D(1024, (3, 3), strides=(1, 1), padding='same', name='conv_16', use_bias=False)(x)
-    x = BatchNormalization(name='norm_16')(x)
-    x = LeakyReLU(alpha=0.1)(x)
-
-    # Layer 17
-    x = Conv2D(512, (1, 1), strides=(1, 1), padding='same', name='conv_17', use_bias=False)(x)
-    x = BatchNormalization(name='norm_17')(x)
-    x = LeakyReLU(alpha=0.1)(x)
-
-    # Layer 18
-    x = Conv2D(1024, (3, 3), strides=(1, 1), padding='same', name='conv_18', use_bias=False)(x)
-    x = BatchNormalization(name='norm_18')(x)
-    x = LeakyReLU(alpha=0.1)(x)
-
-    # Layer 19
-    x = Conv2D(1024, (3, 3), strides=(1, 1), padding='same', name='conv_19', use_bias=False)(x)
-    x = BatchNormalization(name='norm_19')(x)
-    x = LeakyReLU(alpha=0.1)(x)
-
-    # Layer 20
-    x = Conv2D(1024, (3, 3), strides=(1, 1), padding='same', name='conv_20', use_bias=False)(x)
-    x = BatchNormalization(name='norm_20')(x)
-    x = LeakyReLU(alpha=0.1)(x)
-
-    # Layer 21
-    skip_connection = Conv2D(64, (1, 1), strides=(1, 1), padding='same', name='conv_21', use_bias=False)(
-        skip_connection)
-    skip_connection = BatchNormalization(name='norm_21')(skip_connection)
-    skip_connection = LeakyReLU(alpha=0.1)(skip_connection)
-    skip_connection = Lambda(space_to_depth_x2)(skip_connection)
-
-    x = concatenate([skip_connection, x])
-
-    # Layer 22
-    x = Conv2D(1024, (3, 3), strides=(1, 1), padding='same', name='conv_22', use_bias=False)(x)
-    x = BatchNormalization(name='norm_22')(x)
-    x = LeakyReLU(alpha=0.1)(x)
-
+    base_model = VGG16(input_shape=(image_h, image_w, 3), include_top=False, weights='imagenet',
+                       pooling=None)
+    input_image = base_model.input
+    x = base_model.layers[-1].output
     # Layer 23
-    x = Conv2D(num_box * (4 + 1 + num_classes), (1, 1), strides=(1, 1), padding='same', name='conv_23')(x)
+    x = Conv2D(num_box * (4 + 1 + num_classes), (1, 1), strides=(1, 1), padding='same')(x)
     output = Reshape((num_grid, num_grid, 4 + 1 + num_classes))(x)
 
     model = Model(input_image, output)
