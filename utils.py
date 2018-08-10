a54e965f8338c60a7a5c5d9fd1ca1f5e0d5bd1ee
Yang Liu <foamliu@yeah.net>
2018-08-10 20:07:58 +0800

    update

--- utils.py
+++ utils.py
@@ -67,7 +67,7 @@ def yolo_loss(y_true, y_pred):
     loss_xy = tf.reduce_sum(tf.square(box_xy - box_xy_hat) * coord_mask) / (nb_coord_box + 1e-6) / 2.
     loss_wh = tf.reduce_sum(tf.square(box_wh - box_wh_hat) * coord_mask) / (nb_coord_box + 1e-6) / 2.
     loss_conf = tf.reduce_sum(tf.square(box_conf - box_conf_hat) * conf_mask) / (nb_conf_box + 1e-6) / 2.
-    loss_class = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=box_class, logits=box_class_hat)
+    loss_class = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=box_class[..., 0], logits=box_class_hat)
     loss_class = tf.reduce_sum(loss_class * class_mask) / (nb_class_box + 1e-6)
 
     loss = loss_xy + loss_wh + loss_conf + loss_class
