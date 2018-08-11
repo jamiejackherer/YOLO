ba305ca26e419bb483e49af1d8657075eb9f3c8c
Yang Liu <foamliu@yeah.net>
2018-08-11 15:50:06 +0800

    update data gen

--- data_generator.py
+++ data_generator.py
@@ -5,12 +5,13 @@ import numpy as np
 from keras.utils import Sequence
 from pycocotools.coco import COCO
 
-from config import batch_size, image_size, grid_size, num_classes, num_channels, grid_h, grid_w
-from config import train_image_folder, valid_image_folder, train_annot_file, valid_annot_file, catId2idx
+from config import batch_size, image_h, image_w, grid_h, grid_w, num_classes, num_channels, num_box, grid_size, \
+    train_image_folder, valid_image_folder, train_annot_file, valid_annot_file, catId2idx, anchors
+from utils import BoundBox, bbox_iou
 
 
 def get_ground_truth(coco, imgId):
-    gt = np.zeros((num_grid, num_grid, 4 + 1 + num_classes), dtype=np.float32)
+    gt = np.zeros((grid_h, grid_w, num_box, 4 + 1 + num_classes), dtype=np.float32)
     original = coco.loadImgs(ids=[imgId])[0]
     original_height = original['height']
     original_width = original['width']
@@ -19,24 +20,41 @@ def get_ground_truth(coco, imgId):
     for anno in annos:
         category_id = anno['category_id']
         xmin, ymin, width, height = anno['bbox']
-        xmin = 1.0 * xmin * image_size / original_width
-        ymin = 1.0 * ymin * image_size / original_height
-        width = 1.0 * width * image_size / original_width
-        height = 1.0 * height * image_size / original_height
-        x_center = xmin + width / 2
-        y_center = ymin + height / 2
-        cell_x = int(x_center / grid_size)
-        cell_y = int(y_center / grid_size)
-        bx = x_center / grid_size - cell_x
-        by = y_center / grid_size - cell_y
-        bw = width / image_size
-        bh = height / image_size
-        gt[cell_y, cell_x, 0] = 1.0
-        gt[cell_y, cell_x, 1] = bx
-        gt[cell_y, cell_x, 2] = by
-        gt[cell_y, cell_x, 3] = bw
-        gt[cell_y, cell_x, 4] = bh
-        gt[cell_y, cell_x, 5 + catId2idx[category_id]] = 1.0
+        xmin = 1.0 * xmin * image_w / original_width
+        ymin = 1.0 * ymin * image_h / original_height
+        width = 1.0 * width * image_w / original_width
+        height = 1.0 * height * image_h / original_height
+        center_x = xmin + width / 2.
+        center_x = center_x / float(image_w / grid_w)
+        center_y = ymin + height / 2.
+        center_y = center_y / float(image_h / grid_h)
+        cell_x = int(np.floor(center_x))
+        cell_y = int(np.floor(center_y))
+        center_w = width / grid_size
+        center_h = height / grid_size
+        box = [center_x, center_y, center_w, center_h]
+
+        # find the anchor that best predicts this box
+        best_anchor = -1
+        max_iou = -1
+
+        shifted_box = BoundBox(0,
+                               0,
+                               center_w,
+                               center_h)
+
+        for i in range(len(anchors)):
+            anchor = anchors[i]
+            iou = bbox_iou(shifted_box, anchor)
+
+            if max_iou < iou:
+                best_anchor = i
+                max_iou = iou
+
+        # assign ground truth x, y, w, h, confidence and class probs
+        gt[cell_y, cell_x, best_anchor, 0] = 1.0
+        gt[cell_y, cell_x, best_anchor, 1:5] = box
+        gt[cell_y, cell_x, best_anchor, 5 + catId2idx[category_id]] = 1.0
     return gt
 
 
@@ -63,7 +81,7 @@ class DataGenSequence(Sequence):
         i = idx * batch_size
 
         length = min(batch_size, (len(self.imgIds) - i))
-        batch_x = np.empty((length, image_size, image_size, num_channels), dtype=np.float32)
+        batch_x = np.empty((length, image_h, image_w, num_channels), dtype=np.float32)
         batch_y = np.empty((length, grid_h, grid_w, 4 + 1 + num_classes), dtype=np.float32)
 
         for i_batch in range(length):
@@ -72,7 +90,7 @@ class DataGenSequence(Sequence):
             file_name = img['file_name']
             filename = os.path.join(self.image_folder, file_name)
             image_bgr = cv.imread(filename)
-            image_bgr = cv.resize(image_bgr, (image_size, image_size), cv.INTER_CUBIC)
+            image_bgr = cv.resize(image_bgr, (image_h, image_w), cv.INTER_CUBIC)
             image_rgb = image_bgr[:, :, ::-1]
 
             batch_x[i_batch, :, :] = image_rgb / 255.
