f1b8f50b963c1d52a018bb22554e2429535015fc
Yang Liu <foamliu@yeah.net>
2018-08-11 15:01:17 +0800

    update base model filename

--- utils.py
+++ utils.py
@@ -44,15 +44,24 @@ def get_example_numbers():
     return num_train_samples, num_valid_samples
 
 
-def load_weights(model, weight_file):
-    offset = 4
-    all_weights = np.fromfile(weight_file, dtype='float32')
-
-    nb_conv = 23
+class WeightReader:
+    def __init__(self, weight_file):
+        self.offset = 4
+        self.all_weights = np.fromfile(weight_file, dtype='float32')
 
     def read_bytes(self, size):
-        self.offset = offset + size
-        return all_weights[offset - size:offset]
+        self.offset = self.offset + size
+        return self.all_weights[self.offset - size:self.offset]
+
+    def reset(self):
+        self.offset = 4
+
+
+def load_weights(model, weight_file):
+    weight_reader = WeightReader(wt_path)
+
+    weight_reader.reset()
+    nb_conv = 23
 
     for i in range(1, nb_conv + 1):
         conv_layer = model.get_layer('conv_' + str(i))
@@ -62,21 +71,21 @@ def load_weights(model, weight_file):
 
             size = np.prod(norm_layer.get_weights()[0].shape)
 
-            beta = read_bytes(size)
-            gamma = read_bytes(size)
-            mean = read_bytes(size)
-            var = read_bytes(size)
+            beta = weight_reader.read_bytes(size)
+            gamma = weight_reader.read_bytes(size)
+            mean = weight_reader.read_bytes(size)
+            var = weight_reader.read_bytes(size)
 
             weights = norm_layer.set_weights([gamma, beta, mean, var])
 
         if len(conv_layer.get_weights()) > 1:
-            bias = read_bytes(np.prod(conv_layer.get_weights()[1].shape))
-            kernel = read_bytes(np.prod(conv_layer.get_weights()[0].shape))
+            bias = weight_reader.read_bytes(np.prod(conv_layer.get_weights()[1].shape))
+            kernel = weight_reader.read_bytes(np.prod(conv_layer.get_weights()[0].shape))
             kernel = kernel.reshape(list(reversed(conv_layer.get_weights()[0].shape)))
             kernel = kernel.transpose([2, 3, 1, 0])
             conv_layer.set_weights([kernel, bias])
         else:
-            kernel = read_bytes(np.prod(conv_layer.get_weights()[0].shape))
+            kernel = weight_reader.read_bytes(np.prod(conv_layer.get_weights()[0].shape))
             kernel = kernel.reshape(list(reversed(conv_layer.get_weights()[0].shape)))
             kernel = kernel.transpose([2, 3, 1, 0])
             conv_layer.set_weights([kernel])
